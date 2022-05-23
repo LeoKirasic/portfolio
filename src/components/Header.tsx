66921d37e8d3ed92657a9c72d36e1d6c0bfa1d6f
Leo Kirasic <kirasicleo@gmail.com>
2022-05-23 18:51:16 +0200

    Create Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div className="flex justify-center text-4xl mt-14">
+      <div>Leo</div>
+      <div className="text-tokyoNightBlue ml-1">Kirasić</div>
+    </div>
+  );
+}
