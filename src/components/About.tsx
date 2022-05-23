f9be7f7ca4d085368cfda73f864cf6b9de16fc9c
Leo Kirasic <kirasicleo@gmail.com>
2022-05-23 21:15:43 +0200

    Create About component

---
+++ src/components/About.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <p className="flex justify-center mt-1">
+      Web Developer and Open Source Maintainer
+    </p>
+  );
+}
