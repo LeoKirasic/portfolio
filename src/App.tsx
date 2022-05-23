8030cc2301703fd9bd6b3345ca9441422a42256b
Leo Kirasic <kirasicleo@gmail.com>
2022-05-23 21:16:20 +0200

    Render About

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from './components/Header';
+import About from './components/About';
 function App() {
   return (
     <div>
       <Header />
+      <About></About>
     </div>
   );
 }
