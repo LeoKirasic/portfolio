ff8c3a4b788ed153186b3f18e87d43ac00bebb2e
Leo Kirasic <kirasicleo@gmail.com>
2022-05-23 21:16:33 +0200

    Render Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import About from './components/About';
+import Icons from './components/Icons';
 function App() {
   return (
     <div>
       <Header />
       <About></About>
+      <Icons></Icons>
     </div>
   );
 }
