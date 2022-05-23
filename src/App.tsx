ef2d0519b0c2d6163374b0ccd6ec113793a1c135
Leo Kirasic <kirasicleo@gmail.com>
2022-05-23 18:51:35 +0200

    Render Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-
+import Header from './components/Header';
 function App() {
-  return <div className="flex justify-center">Leo Kirasić</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
