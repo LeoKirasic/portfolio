fc160b2fa049e39e3a717286d997939a0364ba18
Leo Kirasic <kirasicleo@gmail.com>
2022-05-25 15:39:15 +0200

    Increase text size

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function About() {
   return (
-    <p className="flex justify-center mt-1">
+    <p className="mt-1 text-md sm:text-xl 2xl:text-2xl">
       Web Developer and Open Source Maintainer
     </p>
   );
