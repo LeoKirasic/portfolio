4d62658d23b932aa28e2244a1c0cbfb7d3ccea99
Leo Kirasic <kirasicleo@gmail.com>
2022-05-25 15:41:00 +0200

    Create projects

---
+++ src/helpers/projects.ts
@@ -0,0 +1,34 @@
+const projects = [
+  {
+    title: `Where's Waldo`,
+    smallImage: require('../assets/project-images/small/waldo.png'),
+    mediumImage: require('../assets/project-images/medium/waldo.png'),
+    largeImage: require('../assets/project-images/large/waldo.png'),
+    technologies: 'Typescript, React, TailwindCSS, Firebase',
+    description: 'A Photo Tagging Application',
+    repo: 'https://github.com/LeoKirasic/photo-tagging-app',
+    live: 'https://photo-tagging-app-27499.firebaseapp.com/',
+  },
+  {
+    title: 'Asgard',
+    smallImage: require('../assets/project-images/small/asgard.png'),
+    mediumImage: require('../assets/project-images/medium/asgard.png'),
+    largeImage: require('../assets/project-images/large/asgard.png'),
+    technologies: 'Typescript, React, TailwindCSS, Firebase',
+    description: 'An Instant Messaging Application',
+    repo: 'https://github.com/LeoKirasic/asgard',
+    live: 'https://asgard-2f1b5.web.app/',
+  },
+  {
+    title: 'Suit Store',
+    smallImage: require('../assets/project-images/small/suit-store.png'),
+    mediumImage: require('../assets/project-images/medium/suit-store.png'),
+    largeImage: require('../assets/project-images/large/suit-store.png'),
+    technologies: 'Javascript, React, TailwindCSS',
+    description: 'An E Commerce Application dedicated to suits',
+    repo: 'https://github.com/LeoKirasic/suit-store',
+    live: 'https://suit-store.netlify.app/shop',
+  },
+];
+
+export default projects;
