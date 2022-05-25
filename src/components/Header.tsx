65113e5a8e87dfb9baec6898feba142b912fcf96
Leo Kirasic <kirasicleo@gmail.com>
2022-05-25 15:39:30 +0200

    Increase text size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div className="flex justify-center text-4xl mt-14">
+    <div className="flex text-5xl mt-8 font-bold">
       <div>Leo</div>
       <div className="text-tokyoNightBlue ml-1">Kirasić</div>
     </div>
