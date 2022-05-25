f653dc9ced175779bb11697936bf1ae8652eca0c
Leo Kirasic <kirasicleo@gmail.com>
2022-05-25 15:30:56 +0200

    Add tokyoNightDeepBlue color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
-    colors: {
-      tokyoNightBlue: '#2274A5',
+    extend: {
+      colors: {
+        tokyoNightBlue: '#2274A5',
+        tokyoNightDeepBlue: '#1a191f',
+      },
     },
   },
   plugins: [],
